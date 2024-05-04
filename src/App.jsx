6e2fb8b2466dc753b174490f61d49765f283851a
Harendra Poripireddy <harendraporipireddy98@gmail.com>
2024-05-04 17:54:36 +0000

    projectsidebar,button,no-project-selected

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import NoProjectSelected from "./componenets/NoProjectSelected";
+import NewProject from "./componenets/NewProject";
+import ProjectSideBar from "./componenets/ProjectSideBar";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSideBar />
+      <NoProjectSelected />
+      {/*<NewProject />*/}
+    </main>
   );
 }
 
